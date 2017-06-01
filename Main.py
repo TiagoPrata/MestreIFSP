dd64df22a4d8277be561affe44284b57364b7b1e
Tiago Prata <prataaa@hotmail.com>
2017-06-01 01:59:36 -0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,19 @@
+import logging
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
+from telegram.ext import CommandHandler
+
+updater = Updater(token='397179894:AAE0Cq2sRmZH7YVz7p-OdqXPS-4wHj5vq4M')
+
+dispatcher = updater.dispatcher
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
